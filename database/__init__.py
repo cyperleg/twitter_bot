98d03e6a7de712c0a2c72edb8d5e59506227bcdc
alrykin <almustalive@gmail.com>
2024-04-20 01:15:31 +0300

    update: recreated database initialisation added tests

---
+++ database/__init__.py
@@ -0,0 +1,201 @@
+from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database
+from sqlalchemy.orm import Session
+from config import DB_LOCATION,DB_NAME
+from database import Base
+
+
+db_engine = create_engine(DB_LOCATION)
+
+# Creating database file if not existed
+def database_create():
+    _engine = create_engine(f"sqlite:///{DB_NAME}.db", echo=True)
+    if not database_exists(_engine.url):
+        print(f"Database file within path {_engine.url} not found. Creating database file {DB_LOCATION}")
+        try:
+            Base.metadata.create_all(_engine)
+            print(f"Database created")
+        except Exception as e:
+            print(f"Error occered: \n {e}")
+
+def create_test_data():
+    with Session(db_engine) as session:
+        # Adding app user
+        new_user = User(
+            app_login = "test_login",
+            app_password = "test_password",
+            subscription_expire_date = 123
+        )
+        session.add(new_user)
+        session.commit() # to get user.id later
+
+        # Adding attachments
+        new_attachment_1 = Attachment(
+            user_id= new_user.id,
+            is_text = 0,
+            text = None,
+            attachment_path = "test_path"
+        )
+        new_attachment_2 = Attachment(
+            user_id= new_user.id,
+            is_text = 1,
+            text = "some test text",
+            attachment_path = None
+        )
+        session.add_all([new_attachment_1, new_attachment_2])
+
+        # Adding app settings
+        new_settings = Settings(
+            user_id = new_user.id,
+            max_tweets = 1000,
+            max_retweets = 500,
+            period_cooldown_minutes = 30,
+            linke_chance = 0.1,
+            react_chance = 0.05,
+            action_delay_seconds = 3,
+            normal_acc_retweets_cap = 1200,
+            premium_acc_retweets_cap = 1500
+        )
+        session.add(new_settings)
+
+        # Adding Twitter accounts
+        new_twitter_account_1 = Twitter_account(
+            user_id = new_user.id,
+            is_premium = 1
+        )
+        new_twitter_account_2 = Twitter_account(
+            user_id = new_user.id,
+            is_premium = 0
+        )
+        session.add_all([new_twitter_account_1, new_twitter_account_2])
+        session.commit() # To use twitter_account.id later
+
+        # Adding Twitter accounts statistics
+        new_statistics_1 = Stats(
+            twitter_account_id= new_twitter_account_1.id,
+            total_msg_sent_num = 123,
+            current_period_msg_sent = 11,
+            total_retweets_num = 321,
+            current_period_retweets_num =12,
+            status = "Working"
+        )
+        new_statistics_2 = Stats(
+            twitter_account_id= new_twitter_account_2.id,
+            total_msg_sent_num = 456,
+            current_period_msg_sent = 22,
+            total_retweets_num = 789,
+            current_period_retweets_num = 33,
+            status = "On hold"
+        )
+        session.add_all([new_statistics_1, new_statistics_2])
+
+        # Adding Twitter accounts auth
+        new_auth_1 = Auth(
+            twitter_account_id = new_twitter_account_1.id,
+            login = "some_login",
+            password = "some_password",
+            auth_token = None
+        )
+        new_auth_2 = Auth(
+            twitter_account_id = new_twitter_account_2.id,
+            login = None,
+            password = None,
+            auth_token = "some_auth_token"
+        )
+        session.add_all([new_auth_1, new_auth_2])
+
+        # Adding Twitter accounts proxy
+        new_proxy_1 = Proxy(
+            twitter_account_id = new_twitter_account_1.id,
+            ip = "192.168.0.0",
+            port = "5000",
+            type = "type 1"
+        )
+        new_proxy_2 = Proxy(
+            twitter_account_id = new_twitter_account_2.id,
+            ip = "192.168.0.1",
+            port = "6868",
+            type = "type 2"
+        )
+        session.add_all([new_proxy_1, new_proxy_2])
+
+        # Adding Twitter accounts groups
+        new_group_1 = Group(
+            twitter_account_id = new_twitter_account_1.id,
+            link = "link 1",
+            previous_msg_check_num = 5,
+            )
+        new_group_2 = Group(
+            twitter_account_id = new_twitter_account_1.id,
+            link = "link 2",
+            previous_msg_check_num = 3,
+            )
+        new_group_3 = Group(
+            twitter_account_id = new_twitter_account_2.id,
+            link = "link 3",
+            previous_msg_check_num = 0,
+            )
+        session.add_all([new_group_1, new_group_2, new_group_3])
+        session.commit() # To use group.id later
+
+        # Adding retweets to Twitter accounts groups
+        new_tweet_1 = Retweet(
+            group_id = new_group_1.id,
+            retweet_id = 123
+        )
+        new_tweet_2 = Retweet(
+            group_id = new_group_1.id,
+            retweet_id = 789
+        )
+        new_tweet_3 = Retweet(
+            group_id = new_group_2.id,
+            retweet_id = 963
+        )
+        session.add_all([new_tweet_1, new_tweet_2, new_tweet_3])
+
+        session.commit()
+
+
+if __name__ == "__main__":  # DB Tests
+    from database import User
+    from database import Attachment
+    from database import Settings
+    from database import Twitter_account
+    from database import Stats
+    from database import Auth
+    from database import Proxy
+    from database import Group
+    from database import Retweet
+
+    #database_create()
+
+    #create_test_data()
+
+    # Testing getting data from the table
+    from sqlalchemy import select
+    with Session(db_engine) as session:
+        selected_user = session.execute(select(User).where(User.app_login == "test_login")).scalar_one_or_none()
+        print(selected_user)
+
+        # Printing attachments
+        for attachment in selected_user.attachments:
+            print(attachment)
+
+        # Printing settings
+        print(selected_user.settings)
+
+        # Printing Twitter accounts
+        for account in selected_user.twitter_accounts:
+            print(account)
+            # Printing Twitter account statistics
+            print(account.stats)
+            # Printing Twitter account auth
+            print(account.auth)
+            # Printing Twitter account proxy
+            print(account.proxy)
+            # Printing Twitter account groups
+            for group in account.groups:
+                print(group)
+                # Print Twitter group retweets
+                for retweet in group.retweets_done:
+                    print(retweet)
--- database/database.py
+++ database/database.py
@@ -1,143 +1,188 @@
-from app.database import db
-from sqlalchemy.orm import backref
+from sqlalchemy import ForeignKey
+from sqlalchemy import Integer, Text, REAL
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import relationship
 
 
-class User(db.Model):
+class Base(DeclarativeBase):
+    pass
+
+class User(Base):
     __tablename__ = "user"
-    __table_args__ = {'extend_existing': True}
-    __bind_key__ = "core_db"
-    id = db.Column(db.Integer, primary_key=True, unique=True)
-    app_login = db.Column(db.String(30), unique=True)
-    app_password = db.Column(db.String(30))
-    subscription_expire_date = db.Column(db.DateTime)
-    attachments = db.relationship('attachment', backref=backref('user',
-                                                                uselist=False,
-                                                                single_parent=True,
-                                                                cascade="all, delete-orphan",
-                                                                passive_deletes=True))
-    twitter_accounts = db.relationship('twitter_account', backref=backref('user',
-                                                                          uselist=False,
-                                                                          single_parent=True,
-                                                                          cascade="all, delete-orphan",
-                                                                          passive_deletes=True))
+    id = mapped_column(Integer, primary_key=True, unique=True)
+    app_login = mapped_column(Text, unique=True)
+    app_password = mapped_column(Text)
+    subscription_expire_date = mapped_column(Integer)
+    attachments = relationship('Attachment', backref='user', cascade="all, delete", passive_deletes=False)
+    settings = relationship('Settings', backref='user', cascade="all, delete", passive_deletes=False)
+    twitter_accounts = relationship('Twitter_account', backref='user', cascade="all, delete", passive_deletes=False)
 
-    settings = db.relationship('settings', backref=backref('user',
-                                                           uselist=False,
-                                                           single_parent=True,
-                                                           cascade="all, delete-orphan",
-                                                           passive_deletes=True))
+    def __repr__(self):
+        return("------------------------USER------------------------\n"+
+              "{0: <30} {1: <30}\n".format("User id:", self.id)+
+              "{0: <30} {1: <30}\n".format("app_login:", self.app_login)+
+              "{0: <30} {1: <30}\n".format("app_password:", self.app_password)+
+              "{0: <30} {1: <30}\n".format("subscription_expire_date:", self.subscription_expire_date)+
+              "{0: <30} {1: <30}\n".format("attachments num:", len(self.attachments))+
+              "{0: <30} {1: <30}\n".format("twitter_accounts num:", len(self.twitter_accounts))+
+              "{0: <30} {1: <30}\n".format("settings num:", len(self.settings))+
+              "-----------------------------------------------------")
 
 
-class Attachment(db.Model):
+class Attachment(Base):
     __tablename__ = "attachment"
-    __table_args__ = {'extend_existing': True}
-    __bind_key__ = "core_db"
-    id = db.Column(db.Integer, primary_key=True, unique=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    is_text = db.Column(db.Boolean)
-    text = db.Column(db.Text)
-    attachment_path = db.Column(db.String(300))
+    id = mapped_column(Integer, primary_key=True, unique=True)
+    user_id = mapped_column(Integer, ForeignKey('user.id', ondelete="CASCADE"))
+    is_text = mapped_column(Integer)
+    text = mapped_column(Text)
+    attachment_path = mapped_column(Text)
+
+    def __repr__(self):
+        return("---------------------ATTACHMENT---------------------\n"+
+              "{0: <30} {1: <30}\n".format("Attachment id:", self.id)+
+              "{0: <30} {1: <30}\n".format("user_id:", self.user_id)+
+              "{0: <30} {1: <30}\n".format("is_text:", self.is_text)+
+              "{0: <30} {1: <30}\n".format("text:", self.text if self.text else " ")+
+              "{0: <30} {1: <30}\n".format("attachment_path:", self.attachment_path if self.attachment_path else " ")+
+              "-----------------------------------------------------")
 
 
-class Settings(db.Model):
+class Settings(Base):
     __tablename__ = "settings"
-    __table_args__ = {'extend_existing': True}
-    __bind_key__ = "core_db"
-    id = db.Column(db.Integer, primary_key=True, unique=True)
-    twitter_account_id = db.Column(db.Integer, db.ForeignKey('twitter_account.id'))
-    max_tweets = db.Column(db.Integer)
-    max_retweets = db.Column(db.Integer)
-    period_cooldown_minutes = db.Column(db.Integer)
-    linke_chance = db.Column(db.Float)
-    react_chance = db.Column(db.Float)
-    action_delay_seconds = db.Column(db.Integer)      # Data got from server
-    normal_acc_retweets_cap = db.Column(db.Integer)   # Data got from server
-    premium_acc_retweets_cap = db.Column(db.Integer)  # Data got from server
+    id = mapped_column(Integer, primary_key=True, unique=True)
+    user_id = mapped_column(Integer, ForeignKey('user.id'))
+    max_tweets = mapped_column(Integer)
+    max_retweets = mapped_column(Integer)
+    period_cooldown_minutes = mapped_column(Integer)
+    linke_chance = mapped_column(REAL)
+    react_chance = mapped_column(REAL)
+    action_delay_seconds = mapped_column(Integer)      # Data got from server
+    normal_acc_retweets_cap = mapped_column(Integer)   # Data got from server
+    premium_acc_retweets_cap = mapped_column(Integer)  # Data got from server
+
+    def __repr__(self):
+        return("----------------------SETTINGS----------------------\n"+
+              "{0: <30} {1: <30}\n".format("Settings id:", self.id)+
+              "{0: <30} {1: <30}\n".format("user_id:", self.user_id)+
+              "{0: <30} {1: <30}\n".format("max_tweets:", self.max_tweets)+
+              "{0: <30} {1: <30}\n".format("max_retweets:", self.max_retweets)+
+              "{0: <30} {1: <30}\n".format("period_cooldown_minutes:", self.period_cooldown_minutes)+
+              "{0: <30} {1: <30}\n".format("linke_chance:", self.linke_chance)+
+              "{0: <30} {1: <30}\n".format("react_chance:", self.react_chance)+
+              "{0: <30} {1: <30}\n".format("action_delay_seconds:", self.action_delay_seconds)+
+              "{0: <30} {1: <30}\n".format("normal_acc_retweets_cap:", self.normal_acc_retweets_cap)+
+              "{0: <30} {1: <30}\n".format("premium_acc_retweets_cap:", self.premium_acc_retweets_cap)+
+              "-----------------------------------------------------")
 
 
-class Twitter_account(db.Model):
+class Twitter_account(Base):
     __tablename__ = "twitter_account"
-    __table_args__ = {'extend_existing': True}
-    __bind_key__ = "core_db"
-    id = db.Column(db.Integer, primary_key=True, unique=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    is_premium = db.Column(db.Boolean)
-    stats = db.relationship('stats', backref=backref('twitter_account',
-                                                     uselist=False,
-                                                     single_parent=True,
-                                                     cascade="all, delete-orphan",
-                                                     passive_deletes=True))
-    auth = db.relationship('auth', backref=backref('twitter_account',
-                                                   uselist=False,
-                                                   single_parent=True,
-                                                   cascade="all, delete-orphan",
-                                                   passive_deletes=True))
-    proxy = db.relationship('proxy', backref=backref('twitter_account',
-                                                     uselist=False,
-                                                     single_parent=True,
-                                                     cascade="all, delete-orphan",
-                                                     passive_deletes=True))
-    groups = db.relationship('group', backref=backref('twitter_account',
-                                                      uselist=False,
-                                                      single_parent=True,
-                                                      cascade="all, delete-orphan",
-                                                      passive_deletes=True))
+    id = mapped_column(Integer, primary_key=True, unique=True)
+    user_id = mapped_column(Integer, ForeignKey('user.id'))
+    is_premium = mapped_column(Integer)
+    stats = relationship('Stats', backref='twitter_account', cascade="all, delete", passive_deletes=False)
+    auth = relationship('Auth', backref='twitter_account', cascade="all, delete", passive_deletes=False)
+    proxy = relationship('Proxy', backref='twitter_account', cascade="all, delete", passive_deletes=False)
+    groups = relationship('Group', backref='twitter_account', cascade="all, delete", passive_deletes=False)
 
+    def __repr__(self):
+        return("-------------------TWITTER ACCOUNT------------------\n"+
+              "{0: <30} {1: <30}\n".format("Twitter_account id:", self.id)+
+              "{0: <30} {1: <30}\n".format("user_id:", self.user_id)+
+              "{0: <30} {1: <30}\n".format("is_premium:", self.is_premium)+
+              "{0: <30} {1: <30}\n".format("stats num:", len(self.stats))+
+              "{0: <30} {1: <30}\n".format("auth num:", len(self.auth))+
+              "{0: <30} {1: <30}\n".format("proxy num:", len(self.proxy))+
+              "{0: <30} {1: <30}\n".format("groups num:", len(self.groups))+
+              "-----------------------------------------------------")
 
-class Stats(db.Model):
+class Stats(Base):
     __tablename__ = "stats"
-    __table_args__ = {'extend_existing': True}
-    __bind_key__ = "core_db"
-    id = db.Column(db.Integer, primary_key=True, unique=True)
-    twitter_account_id = db.Column(db.Integer, db.ForeignKey('twitter_account.id'))
-    total_msg_sent_num = db.Column(db.Integer)
-    current_period_msg_sent = db.Column(db.Integer)
-    total_retweets_num = db.Column(db.Integer)
-    current_period_retweets_num = db.Column(db.Integer)
-    status = db.Column(db.String(30))
+    id = mapped_column(Integer, primary_key=True, unique=True)
+    twitter_account_id = mapped_column(Integer, ForeignKey('twitter_account.id'))
+    total_msg_sent_num = mapped_column(Integer)
+    current_period_msg_sent = mapped_column(Integer)
+    total_retweets_num = mapped_column(Integer)
+    current_period_retweets_num = mapped_column(Integer)
+    status = mapped_column(Text)
+
+    def __repr__(self):
+        return("---------------------STATISTICS---------------------\n"+
+              "{0: <30} {1: <30}\n".format("Statistics id:", self.id)+
+              "{0: <30} {1: <30}\n".format("twitter_account_id:", self.twitter_account_id)+
+              "{0: <30} {1: <30}\n".format("total_msg_sent_num:", self.total_msg_sent_num)+
+              "{0: <30} {1: <30}\n".format("current_period_msg_sent:", self.current_period_msg_sent)+
+              "{0: <30} {1: <30}\n".format("total_retweets_num:", self.total_retweets_num)+
+              "{0: <30} {1: <30}\n".format("current_period_retweets_num:", self.current_period_retweets_num)+
+              "{0: <30} {1: <30}\n".format("status:", self.status)+
+              "-----------------------------------------------------")
 
 
-class Auth(db.Model):
+class Auth(Base):
     __tablename__ = "auth"
-    __table_args__ = {'extend_existing': True}
-    __bind_key__ = "core_db"
-    id = db.Column(db.Integer, primary_key=True, unique=True)
-    twitter_account_id = db.Column(db.Integer, db.ForeignKey('twitter_account.id'))
-    login = db.Column(db.String(30))
-    password = db.Column(db.String(30))
-    auth_token = db.Column(db.String(100))
+    id = mapped_column(Integer, primary_key=True, unique=True)
+    twitter_account_id = mapped_column(Integer, ForeignKey('twitter_account.id'))
+    login = mapped_column(Text)
+    password = mapped_column(Text)
+    auth_token = mapped_column(Text)
+
+    def __repr__(self):
+        return("------------------------AUTH------------------------\n"+
+              "{0: <30} {1: <30}\n".format("Auth id:", self.id)+
+              "{0: <30} {1: <30}\n".format("twitter_account_id:", self.twitter_account_id)+
+              "{0: <30} {1: <30}\n".format("login:", self.login if self.login else " ")+
+              "{0: <30} {1: <30}\n".format("password:", self.password if self.password else " ")+
+              "{0: <30} {1: <30}\n".format("auth_token:", self.auth_token if self.auth_token else " ")+
+              "-----------------------------------------------------")
 
 
-class Proxy(db.Model):
+class Proxy(Base):
     __tablename__ = "proxy"
-    __table_args__ = {'extend_existing': True}
-    __bind_key__ = "core_db"
-    id = db.Column(db.Integer, primary_key=True, unique=True)
-    twitter_account_id = db.Column(db.Integer, db.ForeignKey('twitter_account.id'))
-    ip = db.Column(db.String(16))
-    port = db.Column(db.Integer)
-    type = db.Column(db.String(20))  #TODO do we really need this? how to use?
+    id = mapped_column(Integer, primary_key=True, unique=True)
+    twitter_account_id = mapped_column(Integer, ForeignKey('twitter_account.id'))
+    ip = mapped_column(Text)
+    port = mapped_column(Integer)
+    type = mapped_column(Text)  #TODO do we really need this? how to use?
+
+    def __repr__(self):
+        return("-----------------------PROXY------------------------\n"+
+              "{0: <30} {1: <30}\n".format("Proxy id:", self.id)+
+              "{0: <30} {1: <30}\n".format("twitter_account_id:", self.twitter_account_id)+
+              "{0: <30} {1: <30}\n".format("ip:", self.ip)+
+              "{0: <30} {1: <30}\n".format("port:", self.port)+
+              "{0: <30} {1: <30}\n".format("type:", self.type)+
+              "-----------------------------------------------------")
 
 
-class Group(db.Model):
+class Group(Base):
     __tablename__ = "group"
-    __table_args__ = {'extend_existing': True}
-    __bind_key__ = "core_db"
-    id = db.Column(db.Integer, primary_key=True, unique=True)
-    twitter_account_id = db.Column(db.Integer, db.ForeignKey('twitter_account.id'))
-    link = db.Column(db.String(100))
-    previous_msg_check_num = db.Column(db.Integer)
-    retweets_done = db.relationship('retweet', backref=backref('group',
-                                                                uselist=False,
-                                                                single_parent=True,
-                                                                cascade="all, delete-orphan",
-                                                                passive_deletes=True))
+    id = mapped_column(Integer, primary_key=True, unique=True)
+    twitter_account_id = mapped_column(Integer, ForeignKey('twitter_account.id'))
+    link = mapped_column(Text)
+    previous_msg_check_num = mapped_column(Integer)
+    retweets_done = relationship('Retweet', backref='group', cascade="all, delete", passive_deletes=False)
+
+    def __repr__(self):
+        return("-----------------------GROUP------------------------\n"+
+              "{0: <30} {1: <30}\n".format("Group id:", self.id)+
+              "{0: <30} {1: <30}\n".format("twitter_account_id:", self.twitter_account_id)+
+              "{0: <30} {1: <30}\n".format("link:", self.link)+
+              "{0: <30} {1: <30}\n".format("previous_msg_check_num:", self.previous_msg_check_num)+
+              "{0: <30} {1: <30}\n".format("retweets_done num:", len(self.retweets_done))+
+              "-----------------------------------------------------")
 
 
-class Retweet(db.Model):
+class Retweet(Base):
     __tablename__ = "retweet"
-    __table_args__ = {'extend_existing': True}
-    __bind_key__ = "core_db"
-    id = db.Column(db.Integer, primary_key=True, unique=True)
-    group_id = db.Column(db.Integer, db.ForeignKey('group.id'))
-    retweet_id = db.Column(db.Integer)
+    id = mapped_column(Integer, primary_key=True, unique=True)
+    group_id = mapped_column(Integer, ForeignKey('group.id'))
+    retweet_id = mapped_column(Integer)
+
+    def __repr__(self):
+        return("-----------------------RETWEET-----------------------\n"+
+              "{0: <30} {1: <30}\n".format("Retweet id:", self.id)+
+              "{0: <30} {1: <30}\n".format("group_id:", self.group_id)+
+              "{0: <30} {1: <30}\n".format("retweet_id:", self.retweet_id)+
+              "-----------------------------------------------------")
